b8fbe3156313101d896d465621af60cd80a3823b
danuartha <danuartha.10109013@student.polsub.ac.id>
2024-08-13 22:20:29 +0700

    2nd

--- resources/js/Pages/HomeRoute.vue
+++ resources/js/Pages/HomeRoute.vue
@@ -1,7 +1,6 @@
 <template>
-    <title>CC</title>
     <div>
-        <h1>HOME</h1>
+        <h1>HOME test second commit</h1>
         <router-link to="/test"> Take me to Test page </router-link>
         <button @click.prevent="tiggerEndpoint">Trigger Endpoint</button>
         <p v-if="response">{{ response.data }}</p>
